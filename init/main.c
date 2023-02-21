crdroidandroid/android_kernel_xiaomi_sm6150

init: report fingerprint sensor on boot (for F7B)

init/main.c
@@ -510,6 +510,10 @@ static void __init mm_init(void)
 	pti_init();
 }
 
+#ifdef CONFIG_MACH_XIAOMI_F7B
+int fpsensor;
+#endif
+
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -552,6 +556,11 @@ asmlinkage __visible void __init start_kernel(void)
 	pr_notice("Kernel command line: %s\n", boot_command_line);
 	/* parameters may set static keys */
 	jump_label_init();
+
+#ifdef CONFIG_MACH_XIAOMI_F7B
+	fpsensor = strstr(command_line, "androidboot.fpsensor=fpc") ? 1 : 2;
+#endif
+
 	parse_early_param();
 	after_dashes = parse_args("Booting kernel",
 				  static_command_line, __start___param,

x1337cn/msm-4.14

init/main.c
@@ -538,6 +538,10 @@ static void __init mm_init(void)
 	pti_init();
 }
 
+#ifdef CONFIG_MACH_XIAOMI_VIOLET
+int fpsensor;
+#endif
+
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -573,6 +577,11 @@ asmlinkage __visible void __init start_kernel(void)
 	pr_notice("Kernel command line: %s\n", boot_command_line);
 	/* parameters may set static keys */
 	jump_label_init();
+
+#ifdef CONFIG_MACH_XIAOMI_VIOLET
+	fpsensor = strstr(command_line, "androidboot.fpsensor=fpc") ? 1 : 2;
+#endif
+
 	parse_early_param();
 	after_dashes = parse_args("Booting kernel",
 				  static_command_line, __start___param,